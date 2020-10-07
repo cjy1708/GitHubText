be07ee6bf57c6ec70d652ac86611177fe75fe2a2
1708cjy <2287497792@qq.com>
2020-10-07 10:20:32 +0800

    第一次创建工程（CJY）

---
+++ cmake-build-debug/CMakeFiles/GitHubText.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitHubText.dir/main.c.obj"
+  "GitHubText.exe"
+  "GitHubText.exe.manifest"
+  "GitHubText.pdb"
+  "libGitHubText.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GitHubText.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
